5e177b4dcb3ed78ff385477e2618bc2ada1e4123
USUARIO <USUARIO@DESKTOP-KOEI58B>
2020-12-06 23:34:00 -0500

    inicio de  proyecto

---
+++ src/utilidad/Utilidad.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package utilidad;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Utilidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
